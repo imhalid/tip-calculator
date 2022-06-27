fe55779fb63e5a12e91f8d6a2b3da279162f6545
imhalid <imhalid@icloud.com>
2022-06-28 00:09:53 +0300

    The overall design and responsive design are complete. The calculation is currently unavailable.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 const button = document.querySelectorAll(".button");
 const billAmount = document.querySelector("#bill");
 const numberOfPeople = document.querySelector("#people");
-const custom = document.querySelector("#custom");
+const custom = document.getElementById("custom");
 const totalAmount = document.querySelector("#totalAmount");
 const tipAmount = document.querySelector("#tipAmount");
 
-const bill = billAmount.value;
-const nop = numberOfPeople.value;
-const customPercent = custom.value;
+let bill = billAmount.value;
+let nop = numberOfPeople.value;
+let customPercent = custom.value;
+
+custom.addEventListener("focus", () => {
+  button.forEach((item) => {
+    item.classList.remove("clickButton");
+  });
+});
 
 const buttonClassAdd = (e) => {
   button.forEach((item) => {
@@ -17,23 +23,15 @@ const buttonClassAdd = (e) => {
   e.classList.add("clickButton");
 };
 
-//get button percentage
+//get buttons percentage
 const buttonGetAttr = (e) => {
-  const attr = e.getAttribute("data-value");
-  return attr;
+  const buttonPercent = e.getAttribute("data-value");
+  return buttonPercent;
 };
 
 button.forEach((item) => {
   item.addEventListener("click", (e) => {
     buttonClassAdd(e.target);
+    console.log(buttonGetAttr(e.target));
   });
 });
-
-const updateValue = (e) => {
-  totalAmount.innerHTML = "$" + e.target.value;
-  if (e.target.value.length == 0) {
-    totalAmount.innerHTML = "$0.00";
-  }
-};
-
-billAmount.addEventListener("input", updateValue);
